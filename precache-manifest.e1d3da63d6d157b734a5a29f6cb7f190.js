ed6befbe9331d139fbecf91a04f474f1efcbf48d
Rafiq <raw96@icloud.com>
2020-07-23 13:56:08 -0400

    Updates

---
+++ precache-manifest.e1d3da63d6d157b734a5a29f6cb7f190.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7955181ba701b9cdf431a1fc5c943922",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0a6cb14b3b4eddc9046a",
+    "url": "/monsters-rolodex/static/css/main.96b57514.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a6cb14b3b4eddc9046a",
+    "url": "/monsters-rolodex/static/js/main.4aaa0654.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
